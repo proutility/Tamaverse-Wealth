d19535753770c067bc95fed36a9e418b2a92ac3c
Cakim2026 <ilhamnp22@gmail.com>
2026-04-23 23:38:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -240,6 +240,7 @@ return `
 <div id="dashboard" class="page">
   <div class="mobile-banner">
      <img src="banner.png" alt="Banner">
+     <div class="mobile-banner-text">Dashboard Pro-Tama Finance</div>
   </div>
 
   <div class="mobile-menu-grid">
